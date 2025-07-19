56f25351906e2a608484898ca489dd24a96c1abe
Weiss_GCM <darma20060311@gmail.com>
2025-07-19 17:02:37 +0700

    Create A2.1.cpp

---
+++ C++/A2.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 5;
+  int b = 10;
+  int c = a + b;
+  int d = a * b;
+  cout << "The product of " << a << " and " << b << " is " << d << "." << endl;
+  cout << "The difference of " << a << " and " << b << " is " << (b - a) << "." << endl;
+  cout << "The sum of " << a << " and " << b << " is " << c << "." << endl;
+  return 0;
+}
